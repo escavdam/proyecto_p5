1e1a92acdec9be5c01ec7b7646287dfece7617aa
17alma41 <17alma41@gmail.com>
2024-05-14 15:03:02 +0200

    playerMovement

--- public/scripts/sketch.js
+++ public/scripts/sketch.js
@@ -1,15 +1,60 @@
-const hello = "hello p5 👋"
+let playerX = 0;
+let playerY = 0;
+let playerSpeed = 5;
 
-function setup(){
-    createCanvas(windowWidth, windowHeight);
-    colorMode(HSB, 360, 100, 100, 100);
-    background(255)
+// Bool para registrar el estado de las teclas
+let moveLeft = false;
+let moveRight = false;
+let moveUp = false;
+let moveDown = false;
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
 }
-function draw(){
-    background(360, 30)
-    textSize(42);
-    fill(frameCount%360, 100, 100, 100);
-    stroke(0);
-    strokeWeight(2)
-    text(hello, noise(frameCount/100)*width, noise(frameCount/130)*height);
+
+function draw() {
+  background(220);
+  
+  // Mover al jugador según el estado de las teclas
+  if (moveLeft) {
+    playerX -= playerSpeed;
+  }
+  if (moveRight) {
+    playerX += playerSpeed;
+  }
+  if (moveUp) {
+    playerY -= playerSpeed;
+  }
+  if (moveDown) {
+    playerY += playerSpeed;
+  }
+  
+  // Dibujar al jugador
+  fill(255, 0, 0);
+  translate(width/2, height/2)
+  circle(playerX, playerY, 50);
+}
+
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    moveLeft = true;
+  } else if (keyCode === RIGHT_ARROW) {
+    moveRight = true;
+  } else if (keyCode === UP_ARROW) {
+    moveUp = true;
+  } else if (keyCode === DOWN_ARROW) {
+    moveDown = true;
+  }
+}
+
+function keyReleased() {
+  if (keyCode === LEFT_ARROW) {
+    moveLeft = false;
+  } else if (keyCode === RIGHT_ARROW) {
+    moveRight = false;
+  } else if (keyCode === UP_ARROW) {
+    moveUp = false;
+  } else if (keyCode === DOWN_ARROW) {
+    moveDown = false;
+  }
 }
