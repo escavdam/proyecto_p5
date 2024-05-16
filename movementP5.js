cacccfde49270addc265e9d1f2b0dfa86c267795
Luc626 <lucasst626@gmail.com>
2024-05-16 11:48:11 +0200

    added subway surfer movement

--- movementP5.js
+++ movementP5.js
@@ -1,44 +1,32 @@
-
-let cols, rows;
-let cellSize = 50;
-let playerX = 0.5, playerY = 0.5;
+let playerX, playerY;
+let playerSpeed = 200;
+let originalPlayerX;
 
 function setup() {
-  createCanvas(400, 400);
-  cols = width / cellSize;
-  rows = height / cellSize;
+  createCanvas(800, 800);
+  playerX = width / 2;
+  playerY = height / 1.2;
+  originalPlayerX = playerX;
 }
 
 function draw() {
   background(220);
-  drawGrid();
   drawPlayer();
 }
 
-function drawGrid() {
-  for (let i = 0; i < cols; i++) {
-    for (let j = 0; j < rows; j++) {
-      fill(255);
-      stroke(0);
-      rect(i * cellSize, j * cellSize, cellSize, cellSize);
-    }
-  }
-}
-
 function drawPlayer() {
-  fill(255, 0, 0);
-  ellipse(playerX * cellSize, playerY * cellSize, cellSize, cellSize);
+  fill(255, 1, 1);
+  ellipse(playerX, playerY, 50, 50);
 }
 
-// max y min para que el player 
 function keyPressed() {
-  if (keyCode === LEFT_ARROW) {
-    playerX = max(0.5, playerX - 1);
-  } else if (keyCode === RIGHT_ARROW) {
-    playerX = min(cols - 0.5, playerX + 1);
-  } else if (keyCode === UP_ARROW) {
-    playerY = max(0.5, playerY - 1);
-  } else if (keyCode === DOWN_ARROW) {
-    playerY = min(rows - 0.5, playerY + 1);
+  if (keyCode === LEFT_ARROW && playerX > 0) {
+    playerX -= playerSpeed;
+  } else if (keyCode === RIGHT_ARROW && playerX < width) {
+    playerX += playerSpeed;
   }
+}
+
+function keyReleased() {
+  playerX = originalPlayerX;
 }
