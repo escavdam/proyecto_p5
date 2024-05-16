e6f61d5a0a6aa3e05ddd2e067104b91242df9f3a
17alma41 <17alma41@gmail.com>
2024-05-16 11:49:32 +0200

    update: delete carril move

--- public/scripts/sketch.js
+++ public/scripts/sketch.js
@@ -1,32 +1,25 @@
-let canvasHeight;
-let canvasSpeed = 1;
-let canvasY = 0;
+let canvasWidth;
 let playerStart;
 
 function setup() {
   createCanvas(400, 400);
-  canvasHeight = height / 3; // Dividir el ancho del lienzo en tres carriles
+  canvasWidth = width / 3; // Dividir el ancho del canva en tres carriles
   playerStart = 2; // Iniciar al jugador en el carril medio
 }
 
 function draw() {
   background(220);
   
-  // canvasY += canvasSpeed;
-  // if (canvasY > canvasHeight) {
-  //   canvasY = 0;
-  // }
-
   // Dibujar los carriles
   stroke(0);
   strokeWeight(2);
   for (let i = 1; i <= 3; i++) {
-    line(i * canvasHeight + canvasY, width, i * canvasHeight + canvasY, 0);
+    line(i * canvasWidth, width, i * canvasWidth, 0);
   }
   
   // Dibujar al jugador en su carril
   fill(255, 0, 0);
-  circle(playerStart * canvasHeight - canvasHeight / 2, height / 2, 50);
+  circle(playerStart * canvasWidth - canvasWidth / 2, height / 2, 50);
 }
 
 function keyPressed() {
