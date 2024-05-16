7b03fab15105efdb8f0497951ea7f0c2701c02a0
CantonPo <cantoncorpas@gmail.com>
2024-05-16 10:02:48 +0200

    init 2

--- init.sql
+++ init.sql
@@ -1,22 +1,14 @@
+DROP TABLE IF EXISTS puntuacion_juego;
 
-CREATE DATABASE IF NOT EXISTS puntuacion_juego;
-USE puntuacion_juego;
-
-CREATE TABLE IF NOT EXISTS puntuacion_juego; 
-(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nombre VARCHAR(50),
-    puntos INT
+CREATE TABLE puntuacion_juego(
+    id INTEGER NOT NULL UNIQUE PRIMARY KEY,
+    nombre TEXT NOT NULL UNIQUE,
+    puntuacion INTEGER NOT NULL,
 );
 
-INSERT INTO puntuacion_juego (nombre, puntos) VALUES
-('Alex', 100),
-('Cristian', 150),
-('Makram', 200),
-('Alejandro', 250),
-('Lucas', 300),
-('Javier', 350),
-('Camacho', 400),
-('Idaira', 450);
+INSERT INTO puntuacion_juego(id, nombre, puntuacion) VALUES
+('Canton13', 'Daniel', '1'),
+('Tanque', 'Makram', '10'),
+('Gaditano', 'Alejandro', '2');
 
 SELECT * FROM puntuacion_juego;
