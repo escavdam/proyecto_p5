2b5ec8951edcdd67e50252c3dab695b142b33fcd
17alma41 <17alma41@gmail.com>
2024-05-16 11:39:33 +0200

    feat: movement

--- public/scripts/sketch.js
+++ public/scripts/sketch.js
@@ -1,60 +1,44 @@
-let playerX = 0;
-let playerY = 0;
-let playerSpeed = 5;
-
-// Bool para registrar el estado de las teclas
-let moveLeft = false;
-let moveRight = false;
-let moveUp = false;
-let moveDown = false;
+let canvasHeight;
+let canvasSpeed = 1;
+let canvasY = 0;
+let playerStart;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(400, 400);
+  canvasHeight = height / 3; // Dividir el ancho del lienzo en tres carriles
+  playerStart = 2; // Iniciar al jugador en el carril medio
 }
 
 function draw() {
   background(220);
   
-  // Mover al jugador según el estado de las teclas
-  if (moveLeft) {
-    playerX -= playerSpeed;
-  }
-  if (moveRight) {
-    playerX += playerSpeed;
-  }
-  if (moveUp) {
-    playerY -= playerSpeed;
-  }
-  if (moveDown) {
-    playerY += playerSpeed;
+  // canvasY += canvasSpeed;
+  // if (canvasY > canvasHeight) {
+  //   canvasY = 0;
+  // }
+
+  // Dibujar los carriles
+  stroke(0);
+  strokeWeight(2);
+  for (let i = 1; i <= 3; i++) {
+    line(i * canvasHeight + canvasY, width, i * canvasHeight + canvasY, 0);
   }
   
-  // Dibujar al jugador
+  // Dibujar al jugador en su carril
   fill(255, 0, 0);
-  translate(width/2, height/2)
-  circle(playerX, playerY, 50);
+  circle(playerStart * canvasHeight - canvasHeight / 2, height / 2, 50);
 }
 
 function keyPressed() {
-  if (keyCode === LEFT_ARROW) {
-    moveLeft = true;
-  } else if (keyCode === RIGHT_ARROW) {
-    moveRight = true;
-  } else if (keyCode === UP_ARROW) {
-    moveUp = true;
-  } else if (keyCode === DOWN_ARROW) {
-    moveDown = true;
+  if (keyCode === LEFT_ARROW && playerStart > 1) {
+    playerStart--;
+  } else if (keyCode === RIGHT_ARROW && playerStart < 3) {
+    playerStart++; 
   }
 }
 
-function keyReleased() {
-  if (keyCode === LEFT_ARROW) {
-    moveLeft = false;
-  } else if (keyCode === RIGHT_ARROW) {
-    moveRight = false;
-  } else if (keyCode === UP_ARROW) {
-    moveUp = false;
-  } else if (keyCode === DOWN_ARROW) {
-    moveDown = false;
+function keyReleased(){
+  if (keyCode === RIGHT_ARROW || keyCode === LEFT_ARROW) {
+    playerStart = 2;
   }
 }
