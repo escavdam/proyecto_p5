fe8514790c94f29a1d0992dd7cd15c5985ef6d58
17alma41 <17alma41@gmail.com>
2024-05-23 10:15:05 +0200

    feat: model.js

---
+++ scripts/model.js
@@ -0,0 +1,33 @@
+const fs = require("fs")
+const path = require("path")
+const dbpath = path.join(__dirname, "../db/puntos.db")
+const sqlite = require("better-sqlite3")
+const db = new sqlite(dbpath)
+
+function initDB(){
+    const init = fs.readFileSync(path.join(__dirname, "../db/init.sql"), "utf8")
+    const statements = init.split(";").filter( statement => statement.trim() !== "")
+    statements.forEach(statement => {
+        db.prepare(statement).run()
+    })
+}
+
+function readAll(){
+    return db.prepare("SELECT * FROM puntuaciones").all();
+}
+
+function insertarRecord(nombre, puntos){
+    const statement = db.prepare("INSERT INTO puntuaciones (nombre, puntos) VALUES (?, ?)")
+    statement.run(nombre, puntos)
+}
+
+initDB();
+insertarRecord("pepito", 1000);
+console.log(readAll());
+
+module.exports = {
+    initDB,
+    readAll,
+    insertarRecord
+}
+
