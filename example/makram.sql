44ec3fe4fbdc2de1fac1c4f46061ab97aaef8fc9
MakramElghadir <Mkelghadir@Gmail.com>
2024-05-27 22:52:09 +0200

    correcion

--- example/makram.sql
+++
@@ -1,6 +0,0 @@
--- Création de la table des scores
-CREATE TABLE IF NOT EXISTS subway_surfers_scores (
-    id SERIAL PRIMARY KEY,
-    player_name VARCHAR(100) NOT NULL,
-    score INT NOT NULL
-);
